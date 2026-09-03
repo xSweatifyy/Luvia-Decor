8a3848577c9e8c420e966a4367df6447842fa196
xSweatifyy <136999210+xSweatifyy@users.noreply.github.com>
2026-09-03 14:17:21 +0200

    Protect admin delivery panel behind admin login

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { AdminPage as LegacyAdminPage } from './AdminPageLegacy';
 import { OrderDeliveryPanel } from '../components/OrderDeliveryPanel';
+import { useApp } from '../context/AppContext';
 
-export const AdminPage: React.FC = () => (
-  <>
-    <LegacyAdminPage />
-    <OrderDeliveryPanel />
-  </>
-);
+export const AdminPage: React.FC = () => {
+  const { adminUser } = useApp();
+
+  return (
+    <>
+      <LegacyAdminPage />
+      {adminUser && <OrderDeliveryPanel />}
+    </>
+  );
+};
