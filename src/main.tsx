db14f2bbdda688cfbc2f2b5cc47e4bd5ed6cd571
xSweatifyy <136999210+xSweatifyy@users.noreply.github.com>
2026-09-03 07:35:09 +0200

    fix: restore app rendering on production

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import PickupWidgetBridge from './components/PickupWidgetBridge';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <PickupWidgetBridge />
   </StrictMode>,
 );
