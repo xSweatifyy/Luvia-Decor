c8fb5f2124521c86ba234f11ef662c3121153892
xSweatifyy <136999210+xSweatifyy@users.noreply.github.com>
2026-09-03 19:14:21 +0200

    Show carrier tracking in admin

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -3,6 +3,7 @@ import { AdminPage as LegacyAdminPage } from './AdminPageLegacy';
 import { ShippingCouponManager } from '../components/ShippingCouponManager';
 import { OrderStatusEnhancer } from '../components/OrderStatusEnhancer';
 import { AllCouponsList } from '../components/AllCouponsList';
+import { OrderTrackingManager } from '../components/OrderTrackingManager';
 
 export const AdminPage: React.FC = () => (
   <>
@@ -10,5 +11,6 @@ export const AdminPage: React.FC = () => (
     <LegacyAdminPage />
     <ShippingCouponManager />
     <AllCouponsList />
+    <OrderTrackingManager />
   </>
 );
